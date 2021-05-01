36dc4f91bd86a9685da40649d5db3b0471d43714
蒋敏 <1217302591@qq.com>
2021-05-01 09:59:40 +0800

    add: readline

---
+++ readline/test.js
@@ -0,0 +1,25 @@
+const readline = require('readline')
+const fs = require('fs')
+const file = fs.createReadStream('./message.txt');
+const out = fs.createWriteStream('./another-message.txt');
+
+var index=1;
+out.write('line'+index.toString()+':\n');
+index+=1
+
+var r1 = readline.createInterface({
+  input:file,
+  output:out,
+  terminal:true
+})
+r1.on('line',function (line) {
+  console.log(line);
+  out.write('line'+index.toString()+':\n');
+  index+=1;
+})
+file.on('end',function () {
+  console.log('end')
+})
+r1.on('close',function () {
+  console.log('结束了')
+})
