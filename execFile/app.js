ee9ef1e408a71db76119bd4ce4bd5754d9f64b8b
蒋敏 <jiangmin@up366.com>
2021-05-17 10:19:53 +0800

    add execFile

--- execFile/app.js
+++ execFile/app.js
@@ -5,7 +5,7 @@ var sp1 = cp.execFile('test1.bat',['one','two','three','four'],{
 },function (err,stdout,stderr) {
     if(err){
         console.log('子进程开启失败'+err);
-    //     // process.exit()
+        process.exit()
     }else{
         console.log("子进程标准输出"+stdout.toString())
         sp2.stdin.write(stdout.toString())
