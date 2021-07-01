8ff6c72eec484a8bd510b32dbdcc4b69ba97e856
蒋敏 <jiangmin@up366.com>
2021-07-01 14:23:32 +0800

    fix: 修改版本号

--- test/http.js
+++ test/http.js
@@ -1,11 +1,10 @@
-var http = require('http');
-http.createServer(function (req,res) {
-    res.writeHead(200,{'Content-Type':'text/html'})
-    res.write('<head><meta charset="utf-8"></head>');
-    res.write('<body>');
-    res.write('<div>工作狂和自恋逛</div>')
-    res.write('<div>你好\n</div>')
-    res.end('</body>')
-
-}).listen(1337,"127.0.0.1");
+var http = require('http')
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write('<head><meta charset="utf-8"></head>')
+  res.write('<body>')
+  res.write('<div>工作狂和自恋逛</div>')
+  res.write('<div>你好\n</div>')
+  res.end('</body>')
+}).listen(1337, '127.0.0.1')
 console.log('Servver running at http://127.0.0.1:1337/')
