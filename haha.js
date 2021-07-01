1c1b02cb0a130f0ea13a8662accff23df9b536a7
蒋敏 <jiangmin@up366.com>
2021-07-01 15:19:24 +0800

    fix: 测试修改10000

--- haha.js
+++ haha.js
@@ -1,4 +1,7 @@
-a = [1,2,3,4,5,6,7,8]
-a.forEach(function(item){
-console.log(item)
+const a = [1, 2, 3, 4, 5, 6, 7, 8]
+a.forEach(function (item) {
+  console.log(item)
+})
+a.forEach(function (item) {
+  console.log(item + 1)
 })
